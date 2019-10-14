09b0e16cd18553d557b4f9c66983cd541bc32fd4
estivenLandazury <estiven.landazury@correo.icesi.edu.co>
2019-10-14 18:04:10 -0500

    first commit

---
+++ src/Components/Infovotante.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+import '../Customcss/info.css';
+import Scan from './Scan';
+import { Form } from 'react-bootstrap';
+import 'react-notifications/lib/notifications.css'
+import { NotificationContainer, NotificationManager } from 'react-notifications';
+
+
+
+
+class infovotante extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            checked: false,
+            checked1: false,
+
+        }
+
+        console.log("hola" + this.props)
+    }
+
+    componentDidMount() {
+
+    }
+
+
+    checkedState() {
+        var v = document.getElementById("123");
+        var v1 = document.getElementById("1234");
+
+        if (v.checked == true) {
+            v1.checked = false
+            this.setState({
+                checked: true,
+                checked1: false
+            })
+
+
+
+            console.log("reader " + " estado " + this.state.checked)
+
+        } else if (v.checked == false) {
+            this.setState({
+                checked: false
+            })
+        }
+
+
+    }
+
+    checkedState2() {
+        var v = document.getElementById("123");
+        var v1 = document.getElementById("1234");
+        if (v1.checked == true) {
+            v.checked = false;
+
+            this.setState({
+                checked1: true,
+                checked: false
+
+            })
+            console.log("reader2 " + " estado " + this.state.checked1)
+
+
+
+        } else if (v1.checked == false) {
+            this.setState({
+                checked1: false
+            })
+
+        }
+    }
+
+
+    scanShow() {
+        NotificationManager.info('Información importante', "Ahora puede scanear su QR", 8000)
+
+
+        return <Scan cheked1={this.state.checked} checkedState2={this.state.checked1}></Scan>
+
+    }
+
+
+    render() {
+
+        return (
+
+            <div>
+
+                <h1 className="title">
+                    Información al Votante <br></br>
+                    Elecciones Representante Innovación
+                </h1>
+
+                <div className="container px-lg-5">
+                    <div className="row mx-lg-n5">
+                        <div className="col py-3 px-lg-5 ">
+
+
+                            <div className="card" >
+                                <div className="card-body">
+                                    <h5 className="card-title">Validar Mi voto</h5>
+                                    <p className="card-text">Aqui puedes verificar si tu voto fue contabilizado correctamente.</p>
+                                    <Form.Check type="checkbox" id="123" name="check1" onChange={this.checkedState.bind(this)} label="Seleccioname" />
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col py-3 px-lg-5">
+
+
+                            <div className="card" >
+                                <div className="card-body">
+                                    <h5 className="card-title">¿Por quién voté?</h5>
+                                    <p className="card-text">Aqui puedes verificar por el candidato que realizaste tu voto en la urna.</p>
+                                    <Form.Check type="checkbox" id="1234" name="check2" onChange={this.checkedState2.bind(this)} label="Seleccioname" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                {
+                    this.state.checked == true ? (
+
+                        this.scanShow()
+
+                    ) : ("")
+                }
+
+
+                {
+                    this.state.checked1 == true ? (
+
+                        this.scanShow()
+
+                    ) : ("")
+                }
+
+                <NotificationContainer></NotificationContainer>
+
+
+
+
+
+            </div>
+        );
+    }
+
+
+}
+
+export default infovotante;
---
+++ src/Components/Scan.js
@@ -0,0 +1,157 @@
+import React, { Component } from 'react';
+import QrReader from 'react-qr-reader'
+import '../Customcss/scan.css';
+import 'react-notifications/lib/notifications.css'
+import { NotificationContainer, NotificationManager } from 'react-notifications';
+
+
+
+class scan extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            result: 'No result',
+            URL: "http://192.168.96.37:5000/"
+
+
+
+
+        }
+
+        console.log("hola" + this.props)
+    }
+
+
+    handleScan = data => {
+        if (data) {
+
+            this.setState({
+                result: data
+            })
+
+            let date = { "clave": data };
+
+            let options = {
+                method: 'PUT',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(date)
+            }
+
+
+            if (this.props.cheked1 == false && this.props.checkedState2 == false) {
+
+
+                NotificationManager.warning('Mensaje de Advertencia', 'seleccione alguna opción de consulta', 6000);
+
+
+            }
+
+
+            if (this.props.cheked1 == true) {
+
+                fetch(this.state.URL + "consultarContabilizado", options)
+                    .then(response => response.json())
+                    .then((responseJson) => {
+
+                        if (responseJson.voto == false) {
+                            console.log("no fue contabilizado " + typeof responseJson.voto)
+
+                            NotificationManager.error('Mensaje de Error', 'su voto no fue contabilizado', 5000);
+
+
+                        } else if (responseJson.voto == true) {
+                            console.log(" fue contabilizado " + responseJson.voto)
+
+                            NotificationManager.success('Mensaje Exitoso', " su voto fue contado exitosamente ", 5000)
+
+                        }
+
+
+
+
+                    }).catch(error => NotificationManager.error('Mensaje de Error', 'Error de servidor verifique en el sistema ' + error, 7000)
+                    )
+
+            }
+
+            if (this.props.checkedState2 == true) {
+
+                fetch(this.state.URL + "consultarVoto", options)
+                    .then(response => response.json())
+                    .then((responseJson) => {
+
+                        if (responseJson.voto == "") {
+                            console.log("no votastePor " + responseJson.voto)
+                            NotificationManager.error('Mensaje de Error', 'su voto no fue contabilizado, no se registra candidato pr quien voto', 8000);
+
+
+                        } else if (responseJson.voto !== "") {
+                            console.log("votastePor " + responseJson.voto)
+
+                            NotificationManager.success('Mensaje Exitoso', " su voto fue realizado exitosamente por  " + responseJson.voto, 8000)
+
+                        }
+
+                    }).catch(error => NotificationManager.error('Mensaje de error', 'Error de servidor verifique en el sistema ' + error, 7000)
+                    )
+
+            }
+
+
+
+
+
+            /*NotificationManager.success('Success message', "Su voto fue realizado por " + data);*/
+            console.log("checked1 " + this.props.cheked1 + "   checked2 " + this.props.checkedState2)
+
+        }
+    }
+
+
+    handleError = err => {
+        NotificationManager.error('Error message', err);
+
+        console.error(err)
+
+    }
+
+
+
+    componentDidMount() {
+
+    }
+
+
+
+    render() {
+
+        return (
+
+            <div>
+
+                < div className="Scaner">
+
+                    <QrReader
+                        delay={300}
+                        onError={this.handleError}
+                        onScan={this.handleScan}
+                        style={{ width: '100%' }}
+                    />
+                </ div>
+
+                <NotificationContainer></NotificationContainer>
+
+            </div>
+        );
+    }
+
+
+}
+
+export default scan;
