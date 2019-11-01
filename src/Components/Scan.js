8b4689e0f758efab45988b8a6acad89ec858ddf5
estivenLandazury <estiven.landazury@correo.icesi.edu.co>
2019-11-01 08:54:58 -0500

    update project

--- src/Components/Scan.js
+++ src/Components/Scan.js
@@ -16,16 +16,11 @@ class scan extends Component {
 
         this.state = {
             result: 'No result',
-            URL: "http://192.168.96.37:5000/",
+            /*URL: "http://192.168.96.37:5000/",*/
             show: true,
             checked1: this.props.cheked1,
-            checked2: this.props.checkedState2
-
-            /*URL: "http://e5facf9c.ngrok.io/"*/
-
-
-
-
+            checked2: this.props.checkedState2,
+            URL: "https://b7a74ef9.ngrok.io/"
 
         }
 
